bf9711a1dfe5aa640c3eb6c094e3fa7840fa1d20
Choon Hong Peck <chpeck@gmail.com>
2022-10-16 12:22:59 +0800

    Track links

--- js/home.js
+++ js/home.js
@@ -6,15 +6,6 @@ function showSlides () {
     pause: 3000,
     hoverPause: true
   });
-
-  $('#categories a').click(function (e) {
-    e.preventDefault();
-    $('#categories li.active').removeClass('active');
-    $(this).closest('li').addClass('active');
-    $.get($(this).attr('href'), function (data) {
-      $('#products').html(data);
-    });
-  });
 }
 
 showSlides()
